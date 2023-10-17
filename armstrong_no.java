0f49cf28b040a3ce457f6d8c72f48612c36ebc81
Raoabhi11 <131745014+Raoabhi11@users.noreply.github.com>
2023-10-17 19:24:45 +0530

    Add files via upload

---
+++ armstrong_no.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class armstrong_no {
+    public static int armstrong(int n) {
+        int sum=0;
+      
+
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter any number: ");
+        int n=sc.nextInt();
+    }
+}
